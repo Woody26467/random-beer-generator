4d72924f76a39e2aa8fbf6164517a3b3fff418ec
Andy Woodward <andy.woodward1967@gmail.com>
2023-03-23 09:12:54 +0000

    Get title and description from the data and add to h3 and p

--- js/app.js
+++ js/app.js
@@ -6,7 +6,12 @@ function getData() {
   fetch('https://api.punkapi.com/v2/beers/random')
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      console.log()
+      const name = data[0].name
+      const description = data[0].description
+
+      beerTitle.textContent = name
+      beerDescription.textContent = description
     })
 }
 
