74fccdb8aacd13226408b939565cabf441fe8b60
Andy Woodward <andy.woodward1967@gmail.com>
2023-03-23 09:02:14 +0000

    Add function to get data from api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 const beerButton = document.querySelector('#beer-button')
 const beerTitle = document.querySelector('#random-beer')
 const beerDescription = document.querySelector('#description')
+
+function getData() {
+  fetch('https://api.punkapi.com/v2/beers/random')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+}
+
+beerButton.addEventListener('click', getData)
