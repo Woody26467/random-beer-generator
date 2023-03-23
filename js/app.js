71d0213aa5ad538a5f95a2c6b26ff9371b674691
Andy Woodward <andy.woodward1967@gmail.com>
2023-03-23 10:17:22 +0000

    Add remaining data to app for image and volume

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 const beerButton = document.querySelector('#beer-button')
 const beerTitle = document.querySelector('#random-beer')
 const beerDescription = document.querySelector('#description')
+const beerImage = document.querySelector('#beer-image')
 
 function getData() {
   fetch('https://api.punkapi.com/v2/beers/random')
     .then(response => response.json())
     .then(data => {
-      console.log()
+      console.log(data[0].volume)
       const name = data[0].name
       const description = data[0].description
+      const volumeUnit = data[0].volume.unit
+      const volumeValue = data[0].volume.value
 
-      beerTitle.textContent = name
+      beerTitle.textContent = name + ' | ' + volumeValue + volumeUnit
       beerDescription.textContent = description
+      beerImage.setAttribute('src', data[0].image_url)
     })
 }
+
+getData()
 
 beerButton.addEventListener('click', getData)
